b16796164d66dd2f3b1ea838434d08f26daf23fb
raeditio <conandoil01@gmail.com>
2025-03-20 10:23:26 -0700

    Up to Date

---
+++ icon_drone/icon_drone/shfiles/rspx4.sh
@@ -0,0 +1,10 @@
+sudo chmod 777 /dev/ttyACM0 & sleep 2;
+roslaunch realsense2_camera rs_camera.launch & sleep 1;
+roslaunch fdilink_ahrs ahrs_data.launch & sleep 1;
+roslaunch mavros px4.launch & sleep 1;
+
+# rosrun mavros mavcmd long 511 105 5000 0 0 0 0 0 & sleep 1;
+# rosrun mavros mavcmd long 511 31 5000 0 0 0 0 0 & sleep 1;
+roslaunch vins fast_drone_250.launch & sleep 1;
+rosrun pose_utils odom2pose.py;
+wait;
