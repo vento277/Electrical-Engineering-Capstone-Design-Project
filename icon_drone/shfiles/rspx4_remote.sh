42f1915e6e9db28a85ad5d93603c7abbdefd0cff
ArunarkSingh <105454963+ArunarkSingh@users.noreply.github.com>
2025-06-11 17:09:27 -0700

    Update rspx4_remote.sh

--- icon_drone/shfiles/rspx4_remote.sh
+++ icon_drone/shfiles/rspx4_remote.sh
@@ -3,12 +3,18 @@ export ROS_MASTER_URI=http://<B IP>:11311
 export ROS_IP=<B IP>
 roscore && sleep 5;
 
-roslaunch realsense2_camera rs_camera.launch & sleep 1;
-roslaunch fdilink_ahrs ahrs_data.launch & sleep 1;
-roslaunch mavros px4.launch & sleep 1;
+roslaunch realsense2_camera rs_camera.launch & sleep 2;
+
+# For testing use physical IMU
+roslaunch fdilink_ahrs ahrs_data.launch & sleep 2;
+
+# Launch the Flight Controller 
+roslaunch mavros px4.launch & sleep 2;
 
 # rosrun mavros mavcmd long 511 105 5000 0 0 0 0 0 & sleep 1;
 # rosrun mavros mavcmd long 511 31 5000 0 0 0 0 0 & sleep 1;
-roslaunch vins fast_drone_250.launch & sleep 1;
-rosrun pose_utils odom2pose.py;
+
+# On the PC
+# roslaunch vins fast_drone_250.launch & sleep 1;
+# rosrun pose_utils odom2pose.py;
 wait;
