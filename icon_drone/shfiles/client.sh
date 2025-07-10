76041a89568c8c7c826b6e125ee07c086d35fe2b
raeditio <r2k5d3@gmail.com>
2025-07-10 04:15:00 -0700

    launch scripts optimized

--- icon_drone/shfiles/client.sh
+++ icon_drone/shfiles/client.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
+# Setup ROS environment
 source /opt/ros/noetic/setup.bash
 source ~/Documents/ELEC491_TL101/icon_drone/devel/setup.bash
 
+# Sync system clock
 sudo systemctl restart chrony
 
-pkill -f ros & sleep 3
+# Reset any existing ROS nodes
+pkill -f ros
+sleep 1
 
+# Set ROS networking
 export ROS_MASTER_URI=http://192.168.0.179:11311
 export ROS_HOSTNAME=192.168.0.100
 
+# Start decompression republishers
 rosrun image_transport republish compressed in:=/image1 raw out:=/vins/image1 &
 rosrun image_transport republish compressed in:=/image2 raw out:=/vins/image2 &
 
+# Give republishers a moment to start
 sleep 2
 
-roslaunch vins fast_drone_250.launch
+# Launch VINS and FUEL in parallel
+roslaunch vins fast_drone_250.launch &
+sleep 2
+roslaunch exploration_manager exploration.launch rviz:=false &
--- icon_drone/shfiles/server.sh
+++ icon_drone/shfiles/server.sh
@@ -1,21 +1,34 @@
-
 #!/bin/bash
 
+# Setup ROS environment
 source /opt/ros/noetic/setup.bash
 source ~/ELEC491_TL101/icon_drone/devel/setup.bash
 
+# Set display (for RViz if needed)
 export DISPLAY=:0
 
+# Sync system clock
 sudo systemctl restart chrony
 
-pkill -f ros & sleep 2
+# Reset any existing ROS nodes
+pkill -f ros
+sleep 1
 
-roscore & sleep 2
+# Start ROS core in background
+roscore &
+sleep 2
+
+# Set ROS networking
 export ROS_MASTER_URI=http://192.168.0.179:11311
 export ROS_HOSTNAME=192.168.0.179
 
-roslaunch fdilink_ahrs ahrs_data.launch & sleep 2
-roslaunch mavros px4.launch & sleep 2
+# Launch necessary modules in background
+roslaunch fdilink_ahrs ahrs_data.launch &
+sleep 1
+roslaunch mavros px4.launch &
+sleep 1
+
+# Launch RealSense with optimized config
 roslaunch realsense2_camera rs_camera.launch \
   enable_color:=false \
   enable_depth:=true \
@@ -26,10 +39,13 @@ roslaunch realsense2_camera rs_camera.launch \
   enable_pointcloud:=false \
   enable_sync:=true \
   unite_imu_method:=none \
-  depth_width:=640 depth_height:=480 depth_fps:=15 \
-  infra_width:=640 infra_height:=480 infra_fps:=15 \
-  align_depth:=false \
-  & sleep 2
+  depth_width:=640 depth_height:=480 depth_fps:=30 \
+  infra_width:=640 infra_height:=480 infra_fps:=30 \
+  align_depth:=false &
 
+sleep 2
+
+# Start IR image compression
 rosrun image_transport republish raw in:=/camera/infra1/image_rect_raw compressed out:=/image1 &
 rosrun image_transport republish raw in:=/camera/infra2/image_rect_raw compressed out:=/image2 &
+
