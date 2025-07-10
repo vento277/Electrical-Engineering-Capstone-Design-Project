5ed29c2781aa7bdcc013b99fa7d82c3298483403
raeditio <r2k5d3@gmail.com>
2025-07-10 03:48:56 -0700

    block error resolved in client

--- icon_drone/shfiles/client.sh
+++ icon_drone/shfiles/client.sh
@@ -1,14 +1,18 @@
+#!/bin/bash
+
 source /opt/ros/noetic/setup.bash
 source ~/Documents/ELEC491_TL101/icon_drone/devel/setup.bash
 
 sudo systemctl restart chrony
 
-pkill -f ros & sleep 2
+pkill -f ros & sleep 3
 
 export ROS_MASTER_URI=http://192.168.0.179:11311
 export ROS_HOSTNAME=192.168.0.100
 
-rosrun image_transport republish compressed in:=/image1 raw out:=/vins/image1
-rosrun image_transport republish compressed in:=/image2 raw out:=/vins/image2
+rosrun image_transport republish compressed in:=/image1 raw out:=/vins/image1 &
+rosrun image_transport republish compressed in:=/image2 raw out:=/vins/image2 &
+
+sleep 2
 
 roslaunch vins fast_drone_250.launch
