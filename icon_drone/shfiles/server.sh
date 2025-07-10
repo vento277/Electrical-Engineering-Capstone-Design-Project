50542c75fdbe8c5641da5a4e3a90c49e12ac6787
raeditio <r2k5d3@gmail.com>
2025-07-10 02:34:10 -0700

    compressed image transfer

--- icon_drone/shfiles/server.sh
+++ icon_drone/shfiles/server.sh
@@ -13,6 +13,10 @@ roscore & sleep 2
 export ROS_MASTER_URI=http://192.168.0.179:11311
 export ROS_HOSTNAME=192.168.0.179
 
-roslaunch realsense2_camera rs_camera.launch & sleep 2
 roslaunch fdilink_ahrs ahrs_data.launch & sleep 2
 roslaunch mavros px4.launch & sleep 2
+roslaunch realsense2_camera rs_camera.launch & sleep 2
+
+rosrun image_transport republish raw in:=/camera/infra1/image_rect_raw compressed out:=/camera/infra1/image_rect_raw/compressed
+rosrun image_transport republish raw in:=/camera/infra2/image_rect_raw compressed out:=/camera/infra2/image_rect_raw/compressed
+
